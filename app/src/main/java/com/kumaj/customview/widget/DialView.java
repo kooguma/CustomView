9da66d5048f3e11ce893c7a4f3d0bc7ad4ca318a
lzz <liuzizhang1994@gmail.com>
2016-07-22 14:21:35 +0800

    create DialView

---
+++ app/src/main/java/com/kumaj/customview/widget/DialView.java
@@ -0,0 +1,21 @@
+package com.kumaj.customview.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DialView extends View {
+
+    public DialView(Context context) {
+        super(context);
+    }
+
+    public DialView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DialView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
