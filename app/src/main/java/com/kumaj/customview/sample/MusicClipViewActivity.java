6365a1480e5c006cb927c2d86c8d73cc597b7eb0
lzz <liuzizhang1994@gmail.com>
2019-03-25 10:20:34 +0800

    add animation to MusicClipView & ToningIndicator

--- app/src/main/java/com/kumaj/customview/sample/MusicClipViewActivity.java
+++ app/src/main/java/com/kumaj/customview/sample/MusicClipViewActivity.java
@@ -3,12 +3,34 @@ package com.kumaj.customview.sample;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import com.kumaj.customview.R;
+import io.reactivex.Flowable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import java.util.concurrent.TimeUnit;
+import top.koguma.musicclipview.MusicClipView;
 
 public class MusicClipViewActivity extends AppCompatActivity {
+
+    private MusicClipView musicClipView;
+    private Disposable mDisposable;
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_clip_view);
+        musicClipView = (MusicClipView) findViewById(R.id.music_clip_view);
+        mDisposable = Flowable
+            .interval(10, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext(aLong -> {
+                musicClipView.setDotProgress(aLong / 50.0f);
+            })
+            .subscribe();
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        mDisposable.dispose();
     }
 }
--- app/src/main/java/com/kumaj/customview/sample/ToningIndicatorActivity.java
+++ app/src/main/java/com/kumaj/customview/sample/ToningIndicatorActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.kumaj.customview.R;
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import java.util.concurrent.TimeUnit;
 import top.koguma.toningindicator.ToningIndicator;
@@ -13,12 +14,13 @@ import top.koguma.toningindicator.ToningIndicator;
 public class ToningIndicatorActivity extends AppCompatActivity {
 
     private ToningIndicator indicator;
+    private Disposable mDisposable;
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toning_indicator);
         indicator = (ToningIndicator) findViewById(R.id.indicator);
-        Flowable
+        mDisposable = Flowable
             .interval(1000, TimeUnit.MILLISECONDS)
             .observeOn(AndroidSchedulers.mainThread())
             .doOnNext(aLong -> {
@@ -29,5 +31,10 @@ public class ToningIndicatorActivity extends AppCompatActivity {
                 indicator.setDeltaFrequency((float) frequency);
             })
             .subscribe();
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        mDisposable.dispose();
     }
 }
